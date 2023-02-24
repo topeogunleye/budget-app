15866a5906a8276c585f01727eaad86ada6be7fe
Temitope Ogunleye <topeogunleye1@gmail.com>
2023-02-24 06:44:25 +0100

    Add controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :categories, foreign_key: :author_id
-  has_many :expenditures, foreign_key: :user_id
+         :recoverable, :rememberable, :validatable, :confirmable
+  has_many :expenditures, dependent: :destroy
+  has_many :categories, dependent: :destroy
 
   has_one_attached :image
 
-  validates :name, presence: true
+  validates :name, :email, presence: true
 end
